bfe32e65a2b828ee0c4623202a0c7c4685a62a3f
oszkarpap <scaevola.c@gmail.com>
2019-05-01 10:41:10 +0200

    7.

--- src/com/timbuchalka/Customer.java
+++ src/com/timbuchalka/Customer.java
@@ -1,29 +1,30 @@
 package com.timbuchalka;
 
-import java.util.ArrayList;
-
 /**
- * Created by dev on 4/09/15.
+ * Created by dev on 16/09/15.
  */
 public class Customer {
     private String name;
-    private ArrayList<Double> transactions;
+    private double balance;
 
-    public Customer(String name, double initialAmount) {
+    public Customer(String name, double balance) {
         this.name = name;
-        this.transactions = new ArrayList<Double>();
-        addTransaction(initialAmount);
-    }
-
-    public void addTransaction(double amount) {
-        this.transactions.add(amount);
+        this.balance = balance;
     }
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<Double> getTransactions() {
-        return transactions;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 }
